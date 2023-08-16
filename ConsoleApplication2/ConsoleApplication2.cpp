917b50387eeb2195f95997e748b19bb6852ba69c
vlad <vlad.it.world@mail.ru>
2023-08-16 22:57:29 +0500

    Writing comments in C++

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -3,6 +3,15 @@
 int main()
 {
 
+    //This is a comment
+
+    /*
+        This
+        is
+        a
+        multi-line
+        comment
+    */
 
     std::cout << "I like pizza!" << std::endl;
     std::cout << "It's really good!" << std::endl;
